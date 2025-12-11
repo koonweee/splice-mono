f3518252bdd2ad73dbffc9dde6793e5c98f3a29b
Jeremy Tan <jeremytkw98@gmail.com>
2025-12-10 18:46:02 -0800

    Simplify transaction CRUD to remove account/snapshot side effects
    Transactions are now purely informational - creating, updating, or
    deleting them no longer triggers automatic account balance updates
    or balance snapshot changes. Balance snapshots remain for bank syncs.

--- backend/src/transaction/transaction.module.ts
+++ backend/src/transaction/transaction.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AccountEntity } from '../account/account.entity';
-import { UserModule } from '../user/user.module';
 import { TransactionController } from './transaction.controller';
 import { TransactionEntity } from './transaction.entity';
-import { TransactionListener } from './transaction.listener';
 import { TransactionService } from './transaction.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([TransactionEntity, AccountEntity]),
-    UserModule, // For UserService (to get user's timezone)
-  ],
+  imports: [TypeOrmModule.forFeature([TransactionEntity])],
   controllers: [TransactionController],
-  providers: [TransactionService, TransactionListener],
+  providers: [TransactionService],
   exports: [TransactionService],
 })
 export class TransactionModule {}
