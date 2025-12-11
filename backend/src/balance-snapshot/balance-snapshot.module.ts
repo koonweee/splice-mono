6246475adb03f9d16ce04dd447ac94a3a144cb60
Jeremy Tan <jeremytkw98@gmail.com>
2025-12-10 23:46:32 -0800

    Delete snapshot controller

--- backend/src/balance-snapshot/balance-snapshot.module.ts
+++ backend/src/balance-snapshot/balance-snapshot.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../user/user.module';
-import { BalanceSnapshotController } from './balance-snapshot.controller';
 import { BalanceSnapshotEntity } from './balance-snapshot.entity';
 import { BalanceSnapshotListener } from './balance-snapshot.listener';
 import { BalanceSnapshotService } from './balance-snapshot.service';
@@ -11,7 +10,6 @@ import { BalanceSnapshotService } from './balance-snapshot.service';
     TypeOrmModule.forFeature([BalanceSnapshotEntity]),
     UserModule, // For UserService (to get user's timezone)
   ],
-  controllers: [BalanceSnapshotController],
   providers: [BalanceSnapshotService, BalanceSnapshotListener],
   exports: [BalanceSnapshotService],
 })
