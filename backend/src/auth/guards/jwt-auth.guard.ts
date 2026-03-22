74ea28ad7a46c78a71843ac5f3b9fb10d2bd35c3
Jeremy Tan <jeremytkw98@gmail.com>
2026-03-22 16:09:21 -0700

    feat: allow personal access token auth

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -1,21 +1,50 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { ExtractJwt } from 'passport-jwt';
 import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { SESSION_JWT_ONLY_KEY } from '../decorators/session-jwt-only.decorator';
+import { PersonalAccessTokenService } from '../personal-access-token.service';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+  constructor(
+    private reflector: Reflector,
+    private readonly patService: PersonalAccessTokenService,
+  ) {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
     if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const rawBearer = ExtractJwt.fromAuthHeaderAsBearerToken()(request);
+
+    if (rawBearer && this.patService.isPersonalAccessToken(rawBearer)) {
+      const sessionJwtOnly = this.reflector.getAllAndOverride<boolean>(
+        SESSION_JWT_ONLY_KEY,
+        [context.getHandler(), context.getClass()],
+      );
+
+      if (sessionJwtOnly) {
+        throw new UnauthorizedException();
+      }
+
+      const user = await this.patService.validateToken(rawBearer);
+
+      if (!user) {
+        throw new UnauthorizedException();
+      }
+
+      request.user = user;
       return true;
     }
 
